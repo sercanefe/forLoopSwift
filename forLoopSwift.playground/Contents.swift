5f20757475c3d4a49d8301b489266c0ef233d04b
Sercan Efe Karaman <70342545+sercanefe@users.noreply.github.com>
2021-10-27 22:49:23 +0300

    Add files via upload

---
+++ forLoopSwift.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var fruits = ["apple","banana","orange"]
+
+for i in fruits{
+    print(i)
+}
+
+var myNumbers = [10,20,30,40,50,60]
+
+for number in myNumbers{
+    var sayi = number / 5
+    print(number,"=", sayi)
+}
+
+for myNewInt in 1...5{
+    print(myNewInt*5)
+}
+ 
